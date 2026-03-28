1b678be73e1e0d511fb5d9e5b6e6feb00808dcfa
mykhailolehkobyt-cloud <mykhailo.lehkobyt@oa.edu.ua>
2026-03-28 19:12:46 +0200

    Task 2 Fix

--- Praktika-1-2-3-4-5/Praktika-1-2-3-4-5.cpp
+++ Praktika-1-2-3-4-5/Praktika-1-2-3-4-5.cpp
@@ -1,189 +1,52 @@
-
-#include <chrono>
+#include <cmath>
 #include <iostream>
-#include <cstdlib> 
-#include <ctime>
-#include <windows.h>
-#include <future>
-using namespace std;
 
-void PrintArray(int arr[], int size)
+double Getsum(double arr[], int size)
 {
-	cout << "Згенерований масив| " << endl;
-	for (int i = 0; i < 5; i++)
-	{
-		cout << arr[i] << " ";
-	}
-	for (int i = size - 5; i < size; i++)
-	{
-		cout << arr[i] << " ";
-	}
-}
-
-
-void SortArray(int arr[], int size)
-{
-	for (int i = 1; i < size; i++)
-	{
-		int j = i - 1;
-		int key = arr[i];
-		while (j >= 0 && arr[j] > key)
-		{
-			arr[j + 1] = arr[j];
-			j--;
-		}
-		arr[j + 1] = key;
-	}
-}
-
-
-void CountingSort(int arr[], int size)
-{
-	const int maximum = 501;
-	int sortarray[maximum] = {};
+	double sumabs = 0;
 	for (int i = 0; i < size; i++)
 	{
-		sortarray[arr[i]]++;
+		sumabs += fabs(arr[i]);
 	}
-
-	int index = 0;
-	for (int i = 0; i < maximum; i++)
-	{
-		while (sortarray[i] > 0)
-		{
-			arr[index] = i;
-			++index;
-			sortarray[i]--;
-		}
-
-	}
+	return sumabs;
 }
 
-void QuickSort(int arr[], int first, int last)
+double GetProductOfFractions(double arr[], int size)
 {
-	int k = first;
-	int n = last;
-	int central = arr[(first + last) / 2];
-	while (n >= k)
+	double sort = 1;
+	for (int i = 0; i < size; i++)
 	{
-		while (arr[k] < central)
-		{
-			k++;
-		}
-		while (arr[n] > central)
-		{
-			n--;
-		}
-		if (k <= n)
-		{
-			swap(arr[k], arr[n]);
-			k++;
-			n--;
-		}
+		sort = sort * (1.0 / arr[i]);
 	}
-	if (first < n)
-	{
-		QuickSort(arr, first, n);
-	}
-	if (k < last)
-	{
-		QuickSort(arr, k, last);
-	}
-}
-
-void asinhron(int arr[], int first, int last)
-{
-	int k = first;
-	int n = last;
-	int central = arr[(first + last) / 2];
-	while (n >= k)
-	{
-		while (arr[k] < central)
-		{
-			k++;
-		}
-		while (arr[n] > central)
-		{
-			n--;
-		}
-		if (k <= n)
-		{
-			swap(arr[k], arr[n]);
-			k++;
-			n--;
-		}
-	}
-	if (last - first > 10000);
-	{
-		future<void> left_half;
-	}
-	if (first < n)
-	{
-		auto left_half = std::async(std::launch::async, asinhron, arr, first, n);
-	}
-	if (k < last)
-	{
-		asinhron(arr, k, last);
-	}
+	return sort;
 }
 
 
-void GenerateArray(int arr[], int size)
-{
-	int min = 50;
-	int max = 500;
-	for (int i = 0; i < size; i++) {
-
-		arr[i] = rand() % (max - min + 1) + min;
-
-
-	}
-}
 
 int main()
 {
-	SetConsoleCP(65001);
-	SetConsoleOutputCP(65001);
-	srand(time(NULL));
+	using namespace std;
+	double A[6] = { -2.3, 6.2, 5.8, -3.4, 7.1, 0.05 };
+	double B[6] = { 3.0, -2.3, 4.1, 2.5, 6.8, 4.5 };
 
+	double X = Getsum(A, 6);
+	double Y = Getsum(B, 6);
 
-	int sizes[] = { 18, 160, 1024, 4096, 32600, 128000 };
-	int numSizes = 6;
-	int choise;
-	cout << "Введіть яким методом хочете посортувати" << endl << "1 Швидке сортування " << endl << "2 Сортування підрахунком" << endl << "3 Сортування вставкою" << endl << " 4	Асинхронне швидке сортування " << endl;
-	cin >> choise;
-	for (int i = 0; i < numSizes; i++)
+	double a = GetProductOfFractions(A, 6);
+	double b = GetProductOfFractions(B, 6);
+
+	double p = (a + b) / (X - Y);
+	double W;
+
+	if (p > 0)
 	{
-		int currentsize = sizes[i];
-		int* dinamik = new int[currentsize];
-
-		GenerateArray(dinamik, currentsize);
-		auto start = chrono::high_resolution_clock::now();
-		switch (choise)
-		{
-		case 1:
-			QuickSort(dinamik, 0, currentsize - 1);
-			break;
-		case 2:
-			CountingSort(dinamik, currentsize);
-			break;
-		case 3:
-			SortArray(dinamik, currentsize);
-			break;
-		case 4:
-			asinhron(dinamik, 0, currentsize - 1);
-			break;
-		default:
-			cout << "Ясно ж сказано, ввести 1 2 або 3 " << endl;
-			break;
-		}
-		auto end = chrono::high_resolution_clock::now();
-		PrintArray(dinamik, currentsize);
-		delete[] dinamik;
-		chrono::duration<double, milli> time_taken = end - start;
-		cout << "Час сортування " << time_taken.count() << " мс" << endl;
+		W = a * X + Y;
 	}
+	else
+	{
+		W = b * X + Y;
+	}
+	cout << "Result W: " << W << endl;
 
-
-
+	return 0;
 }
